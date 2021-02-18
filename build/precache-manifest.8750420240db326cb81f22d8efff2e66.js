aeb5c389956429faa02884652cecaddacabc437d
franciskolms <fkolms@gmail.com>
2021-02-18 16:08:18 +0900

    first commit

---
+++ build/precache-manifest.8750420240db326cb81f22d8efff2e66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf6766ecbcf0a50961315f088f1d9dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0976472c17d6727b2d4b",
+    "url": "/static/css/main.c1de3eb9.chunk.css"
+  },
+  {
+    "revision": "81b9bf487963f6e762e3",
+    "url": "/static/js/2.8c25ecf2.chunk.js"
+  },
+  {
+    "revision": "851820dc40bca3bcaf80cf6dd26069f9",
+    "url": "/static/js/2.8c25ecf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0976472c17d6727b2d4b",
+    "url": "/static/js/main.d319fe32.chunk.js"
+  },
+  {
+    "revision": "92140938f21271b78561",
+    "url": "/static/js/runtime-main.f1889a16.js"
+  }
+]);
